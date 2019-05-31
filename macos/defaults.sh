0888f9dc4135f12316a5d9c87ad44c229ccd364e
kethomassen <kethomassen@gmail.com>
2019-05-31 12:12:37 +1000

    More mac defaults

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -20,6 +20,19 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 # dont show recents in dock
 defaults write com.apple.dock show-recents -bool false
 
+# Text selection in quick look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# save screenshots to ~/Desktop/Screenshots
+defaults write com.apple.screencapture location ~/Desktop/Screenshots
+
+# expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# plain text by default in TextEdit
+defaults write com.apple.TextEdit RichText -int 0
+
 # Hot corners
 # Bottom left: sleep. Bottom right: Mission  control. 
 defaults write com.apple.dock wvous-br-corner -int 2
